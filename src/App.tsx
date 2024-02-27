2420b072032816175affb7517ea121ba6effc408
varun7654 <varun7654@outlook.com>
2024-02-27 15:57:35 -0500

    some testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+function Header() {
+    return (
+        <h1 className = "App-header">Header</h1>
+    )
+}
+
+function Problem() {
+    return (
+        <h2>Problem</h2>
+    )
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Problem />
     </div>
   );
 }
