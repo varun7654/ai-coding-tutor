eede9b8ecd8d6cd1229f366a8522d60460ed9930
Anisha Parida <anishalata@gmail.com>
2024-03-13 20:53:40 -0400

    Added a new home page using router

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import 'highlight.js/styles/atom-one-light.min.css';
 import {Problem} from "./problem/Problem";
+import Home from './Home'; // import the Home component
 
-function Header() {
+export function Header() {
     return (
         <h1 className = "App-header">Header</h1>
     )
 }
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Problem id={window.location.href + ".md"} />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" Component={Home} />
+                    <Route path="/problem" Component={() => <Problem id={window.location.href + ".md"} />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+//<Link to ="/main">Main</Link>
+import React from "react";
+import "./index.css"
+import {Header} from "./App";
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
+export default Home;
