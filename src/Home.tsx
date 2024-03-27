bf0866b3cc8283dc411d9cfe9751e3c730bd1db4
Anisha Parida <anishalata@gmail.com>
2024-03-27 17:22:29 -0400

    Used a map to group the data under the folder name

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,26 @@
 import React, {useEffect, useState} from "react";
-import "./index.css"
-import {Link} from "react-router-dom"; // Import Link from react-router-dom
+import "./Home.css"
+import {Link} from "react-router-dom";
 
 class ProblemData {
     problemName: string = "";
     id: string = "";
 }
 
+function getCategoryFromFilePath(id: string): string {
+    if (!id) {
+        return 'nothing';
+    }
+    const parts = id.split('/');
+    if (parts.length < 2) {
+        return 'nothing';
+    }
+    return parts[parts.length - 2];
+}
+
 function Home() {
     const [data, setData] = useState(null as ProblemData[] | null);
+    const [groupedData, setGroupedData] = useState(new Map<string, ProblemData[]>());
 
     useEffect(() => {
         fetch(process.env.PUBLIC_URL + "/problem_locations.json")
@@ -16,10 +28,19 @@ function Home() {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
-                return await (response.json()) as ProblemData[]// Use text() instead of json() as we're fetching a markdown file
+                return await (response.json()) as ProblemData[]
             })
             .then(data => {
                 setData(data);
+                const map = new Map<string, ProblemData[]>();
+                data.forEach(item => {
+                    const category = getCategoryFromFilePath(item.id);
+                    if (!map.has(category)) {
+                        map.set(category, []);
+                    }
+                    map.get(category)!.push(item);
+                });
+                setGroupedData(map);
             })
             .catch(error => {
                 console.error('There has been a problem with your fetch operation:', error);
@@ -29,11 +50,14 @@ function Home() {
     return (
         <div>
             <h1>Hi and welcome to our page</h1>
-            {/* Map over the data array and generate JSX elements for each item */}
-            {data && data.map((item, index) => (
-                <div key={index}>
-                    <h2>{item.problemName}</h2>
-                    <Link to={"/Problem" + item.id}>Go to Problem Page</Link>
+            {Array.from(groupedData.entries()).map(([category, problems]) => (
+                <div key={category}>
+                    <h2>{category}</h2>
+                    {problems.map((item, index) => (
+                        <div key={index}>
+                            <Link to={"/Problem" + item.id} className={'problem-link'} ><h3>{item.problemName}</h3></Link>
+                        </div>
+                    ))}
                 </div>
             ))}
         </div>
