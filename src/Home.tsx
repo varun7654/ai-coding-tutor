4e32c5fed6576209b02737b58ff909e393b780f9
Anisha Parida <anishalata@gmail.com>
2024-03-31 23:22:21 -0400

    updated colors and design on home

--- src/Home.tsx
+++ src/Home.tsx
@@ -38,26 +38,55 @@ function Home() {
     }, []);
 
 
+    type ProblemSectionProps = {
+        index: number;
+        children: React.ReactNode;
+    };
+
+    function ProblemSection({index, children}: ProblemSectionProps) {
+        let color = getColorForProblemSection(index);
+        let color2 = getColorForProblemSection(index + 1);
+        return (
+            <div
+                style={{background: color}}>
+                {children}
+                <div style={{background: color2}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 720 72" className="pb-5">
+                        <path fill={color}
+                              d="M0,60.56c55.63-30.1,102.12-38.64,133.59-41.06c37.46-2.88,94.56,7.7,207.25,28.99
+                            c72.83,13.76,91.25,18.65,123.67,12.56c55.51-10.42,56.68-35.11,104.35-37.2c52.67-2.31,79.26,26.62,124.64,10.63
+                            c17.13-6.04,29.35-15.8,36.71-22.71L720,0H0V60.56z"></path>
+                    </svg>
+                </div>
+            </div>
+        );
+    }
+
     function getProblemFolderAsJSX(data: (ProblemData | ProblemDirData)[]) {
-        return <div>
-            {data.map((item, index) => {
-                if ('directory' in item) {
-                    return <div key={index}>
-                        <div className="pb-5">
+        return data.map((item, index) => {
+            if ('directory' in item) {
+                return (
+                    <ProblemSection key={index} index={index}>
+                        <div className={"pb-5 ml-5 text-basically-black w-1/2"}>
                             <div className="text-3xl font-bold"> {item.meta.title} </div>
                             <div className="text-lg"> {item.meta.description} </div>
                             <div className="pl-4"> {getProblemFolderAsJSX(item.files)} </div>
                         </div>
-                    </div>
-                } else {
-                    return <div key={index}>
-                        <Link to={"/Problem" + item.problemId} className={'text-bright-blue underline text-lg'}>
-                            {item.problemName}
-                        </Link>
-                    </div>
-                }
-            })}
-        </div>
+                    </ProblemSection>
+                );
+            } else {
+                return <div key={index}>
+                    <Link to={"/Problem" + item.problemId} className={'text-bright-blue underline text-lg'}>
+                        {item.problemName}
+                    </Link>
+                </div>
+            }
+        });
+    }
+
+    function getColorForProblemSection(index: number): string {
+        const colors = ['#f8e6f7', '#e6f7f8', '#f7f8e6', '#e6f8f7', '#f7e6f8', '#f8f7e6'];
+        return colors[index % colors.length];
     }
 
     return (
@@ -108,10 +137,20 @@ function Home() {
                              alt="User going back to the problem to fix the issue"/>
                     </div>
                 </div>
-                <div id="problems" className="text-5xl font-bold mt-8">Problems</div>
-                <div className="w-2/3 pb-96">
-                    {getProblemFolderAsJSX(data?.files || [])}
-                </div>
+
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 720 72" className="pb-5 bg-[#f8e6f7]">
+                <path fill={"#1C1F23"}
+                      d="M0,60.56c55.63-30.1,102.12-38.64,133.59-41.06c37.46-2.88,94.56,7.7,207.25,28.99
+                        c72.83,13.76,91.25,18.65,123.67,12.56c55.51-10.42,56.68-35.11,104.35-37.2c52.67-2.31,79.26,26.62,124.64,10.63
+                        c17.13-6.04,29.35-15.8,36.71-22.71L720,0H0V60.56z"></path>
+            </svg>
+            <div id="problems" className="text-5xl font-bold pt-8 bg-[#f8e6f7] text-basically-black pl-4 ">Problems
+            </div>
+
+            <div className="w-full">
+                {getProblemFolderAsJSX(data?.files || [])}
+
             </div>
         </div>
     )
