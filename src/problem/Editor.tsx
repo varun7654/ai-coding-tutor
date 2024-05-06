fd735e4c5f115abf4a05db28944e6e5106e844fc
varun7654 <varun7654@outlook.com>
2024-05-06 02:57:34 -0700

    small adjustments to the editor

--- src/problem/Editor.tsx
+++ src/problem/Editor.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import AceEditor from "react-ace";
 
-import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-github_dark";
 import "ace-builds/src-noconflict/ext-language_tools";
+
 
 // lang: string, onChange: (value: string) => void, defaultValue: string = ""
 export default function Editor(
@@ -12,16 +13,6 @@ export default function Editor(
         onChange: (value: string) => void,
         defaultValue: string
     }) {
-
-    try {
-        require(`ace-builds/src-noconflict/mode-${lang}`);
-        require(`ace-builds/src-noconflict/snippets/${lang}`);
-    } catch (e) {
-        console.log(`Language ${lang} not supported. Assuming js`, e)
-        lang = "javascript";
-        require(`ace-builds/src-noconflict/mode-${lang}`);
-        require(`ace-builds/src-noconflict/snippets/${lang}`);
-    }
 
     return <AceEditor
         placeholder={""}
@@ -36,7 +27,7 @@ export default function Editor(
             useWorker: false,
             enableBasicAutocompletion: true,
             enableLiveAutocompletion: true,
-            enableSnippets: true,
+            enableSnippets: false,
             showLineNumbers: true,
             tabSize: 2,
             highlightActiveLine: false
